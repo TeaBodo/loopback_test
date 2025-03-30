ac4643a0248943b12a6b8804546c6cf0dfe389f8
TeaBodo <bodova.tea@gmail.com>
2025-03-30 15:19:37 +0200

    Set up project to 80MHz input clock - loopback not working

--- src/top.v
+++ src/top.v
@@ -18,6 +18,7 @@ module top(
 //-------------------------------------------
 wire clk_160m;
 wire clk_320m;
+wire clk_80m;
 wire main_pll_locked;
 wire rst;
 
@@ -25,6 +26,7 @@ main_clk inst_main_clk(
 // Clock out ports
     .clk_320m(clk_320m),     // output clk_320m
     .clk_160m(clk_160m),     // output clk_160m
+    .clk_80m(clk_80m),     // output clk_80m
     // Status and control signals
     .reset(1'b0),          // input reset
     .locked(main_pll_locked),        // output locked
@@ -69,7 +71,7 @@ HPIO_TX inst_hpio_tx (
   .pll0_clkout0(tx_pll0_clkout0),                                  // output wire pll0_clkout0
   .pll0_clkout1(tx_pll0_clkout1),                                     // output wire pll0_clkout1
   .rst(1'b0),                                                      // input wire rst
-  .clk(clk_160m),                                                  // input wire clk_p // input wire clk_n
+  .clk(clk_80m),                                                  // input wire clk_p // input wire clk_n
   .pll0_locked(tx_pll0_locked),                                    // output wire pll0_locked
   .bg1_pin10_clk_p_23(CLK_TX_P),                                    // output wire bg1_pin10_clk_p_23
   .data_from_fabric_bg1_pin10_clk_p_23(8'b10101010),  // input wire [7 : 0] data_from_fabric_bg1_pin10_clk_p_23
@@ -106,10 +108,10 @@ wire [7:0] data_to_fabric_clk_p;
 
 HPIO_RX inst_hpio_rx (
   .fifo_rd_data_valid(rx_fifo_rd_data_valid),                                // output wire fifo_rd_data_valid
-  .fifo_rd_clk_21(clk_160m),                                        // input wire fifo_rd_clk_21
-  .fifo_rd_clk_22(clk_160m),                                        // input wire fifo_rd_clk_22
-  .fifo_rd_clk_26(clk_160m),                                        // input wire fifo_rd_clk_26
-  .fifo_rd_clk_27(clk_160m),                                        // input wire fifo_rd_clk_27
+  .fifo_rd_clk_21(clk_80m),                                        // input wire fifo_rd_clk_21
+  .fifo_rd_clk_22(clk_80m),                                        // input wire fifo_rd_clk_22
+  .fifo_rd_clk_26(clk_80m),                                        // input wire fifo_rd_clk_26
+  .fifo_rd_clk_27(clk_80m),                                        // input wire fifo_rd_clk_27
   .fifo_empty_21(rx_fifo_empty_21),                                          // output wire fifo_empty_21
   .fifo_empty_22(rx_fifo_empty_22),                                          // output wire fifo_empty_22
   .fifo_empty_26(rx_fifo_empty_26),                                          // output wire fifo_empty_26
@@ -128,7 +130,7 @@ HPIO_RX inst_hpio_rx (
   .pll0_clkout0(rx_pll0_clkout0),                                            // output wire pll0_clkout0
   .pll0_clkout1(rx_pll0_clkout1),                                            // output wire pll0_clkout1
   .rst(1'b0),                                                              // input wire rst
-  .clk(clk_160m),                                                              // input wire clk
+  .clk(clk_80m),                                                              // input wire clk
   .pll0_locked(rx_pll0_locked),                                              // output wire pll0_locked
   .bg1_pin0_nc(bg1_pin0_nc),                                              // input wire bg1_pin0_nc
   .bg1_pin8_data_p_21(RX_P),                                // input wire bg1_pin8_data_p_21
